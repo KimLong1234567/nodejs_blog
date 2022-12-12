a2bf7875c50a7df1d97040821bd93ec2f506023a
sfeak <huyb1910644>
2022-12-12 18:31:21 +0700

    Template engine

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const {engine, create, ExpressHandlebars} = require('express-handlebars');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const hbs = create({ /* config */ });
+const port = 3000;
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
